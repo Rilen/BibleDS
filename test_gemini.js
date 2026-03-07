892a43b53b2773c854a9b3185ff106a8fbf3c77c
Rilen Lima <rilen.lima@gmail.com>
2026-03-07 15:06:55 -0300

    chore: cleanup API test scripts and sync caches

--- test_gemini.js
+++
@@ -1,8 +0,0 @@
-fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAaYoRVZR1KdkrcSajQDun3HD_ntUSacNI", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ contents: [{ parts: [{ text: "Quem é Jesus?" }] }] })
-})
-    .then(r => r.json())
-    .then(j => console.log(JSON.stringify(j, null, 2)))
-    .catch(console.error);
--- test_gemini2.js
+++
@@ -1,4 +0,0 @@
-fetch("https://generativelanguage.googleapis.com/v1beta/models?key=AIzaSyAaYoRVZR1KdkrcSajQDun3HD_ntUSacNI")
-    .then(r => r.json())
-    .then(j => console.log(JSON.stringify(j, null, 2)))
-    .catch(console.error);
